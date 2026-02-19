5fde6f5e55b794f4a533ec3157efdd74e2da9beb
aathif12 <aathifahamad4@gma.com>
2026-02-19 19:08:44 +0530

    feat(frontend): Implement client-side caching in BookService for instant navigation

--- book-manager-frontend/src/app/services/book.service.ts
+++ book-manager-frontend/src/app/services/book.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { Book } from '../models/book.model';
 
 @Injectable({
@@ -8,26 +9,49 @@ import { Book } from '../models/book.model';
 })
 export class BookService {
   private readonly apiUrl = 'http://localhost:5199/api/books';
+  private books: Book[] = [];
 
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<Book[]> {
-    return this.http.get<Book[]>(this.apiUrl);
+    if (this.books.length > 0) {
+      return of(this.books);
+    }
+    return this.http.get<Book[]>(this.apiUrl).pipe(
+      tap(books => this.books = books)
+    );
   }
 
   getById(id: number): Observable<Book> {
+    const cachedBook = this.books.find(b => b.id === id);
+    if (cachedBook) {
+      return of(cachedBook);
+    }
     return this.http.get<Book>(`${this.apiUrl}/${id}`);
   }
 
   create(book: Omit<Book, 'id'>): Observable<Book> {
-    return this.http.post<Book>(this.apiUrl, book);
+    return this.http.post<Book>(this.apiUrl, book).pipe(
+      tap(newBook => this.books.push(newBook))
+    );
   }
 
   update(id: number, book: Omit<Book, 'id'>): Observable<Book> {
-    return this.http.put<Book>(`${this.apiUrl}/${id}`, book);
+    return this.http.put<Book>(`${this.apiUrl}/${id}`, book).pipe(
+      tap(updatedBook => {
+        const index = this.books.findIndex(b => b.id === id);
+        if (index !== -1) {
+          this.books[index] = updatedBook;
+        }
+      })
+    );
   }
 
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`).pipe(
+      tap(() => {
+        this.books = this.books.filter(b => b.id !== id);
+      })
+    );
   }
 }
